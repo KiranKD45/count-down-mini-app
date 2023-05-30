5b2cb0b20caf5894d4ac2cbe89d609b70b20be67
kiran madiwalar <kiranm81040@gmail.com>
2023-05-30 12:36:34 +0530

    countdown added

---
+++ script.js
@@ -0,0 +1,31 @@
+const daysElement    = document.getElementById("days")
+const hoursElement   = document.getElementById("hours")
+const minutesElement = document.getElementById("minutes")
+const secondsElement = document.getElementById("seconds")
+
+
+const newYear = "1 jan 2024";
+function countdown(){
+    const newYearDate = new Date(newYear);
+    const currentDate = new Date();
+
+    const totalSeconds =  (newYearDate -currentDate) / 1000;
+    const days = Math.floor(totalSeconds/ 3600 /24);
+    const hours = Math.floor(totalSeconds / 3600) % 24 ;
+    const minutes = Math.floor(totalSeconds/60)%60;
+    const seconds = Math.floor(totalSeconds)% 60;
+
+    daysElement.innerHTML = formatTime(days);
+    hoursElement.innerHTML = formatTime(hours);
+    minutesElement.innerHTML = formatTime(minutes);
+    secondsElement.innerHTML = formatTime(seconds);
+
+    function formatTime (time){
+        return time < 10 ? `0${time}` :  time;
+    }
+     
+    console.log(days, hours ,minutes , seconds);
+
+}
+countdown();
+setInterval(countdown, 1000);
